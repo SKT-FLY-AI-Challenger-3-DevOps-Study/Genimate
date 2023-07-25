5ecf4cb231c38a96a495541a2f65901774d958d3
jeongiin <jeongiin@naver.com>
2023-07-25 11:19:03 +0900

    [✨ ADD]블랙 이미지 체크 함수 추가

--- AI/utils.py
+++ AI/utils.py
@@ -1,4 +1,5 @@
 import torch
+from PIL import Image
 
 def device_loader():
     print(torch.cuda.is_available())
@@ -13,3 +14,15 @@ def device_loader():
         #     device = torch.device('cpu')
             
     return device
+
+
+def is_black_image(img):
+    try:
+        img_data = img.getdata()
+        for pixel in img_data:
+            if pixel != (0, 0, 0):  # Check for non-black pixels (considering RGBA)
+                return False
+        return True
+    except Exception as e:
+        print("Error:", e)
+        return False
