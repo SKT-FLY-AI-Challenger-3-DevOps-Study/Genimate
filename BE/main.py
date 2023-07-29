dfa47df131e13fa23c7493ef5ec67dfbf631453b
jjaegii <hn06038@gmail.com>
2023-07-29 11:16:47 +0900

    [🔨 FIX] set reloader to False

--- BE/main.py
+++ BE/main.py
@@ -29,4 +29,4 @@ async def index():
     return {"message": "Hello World"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True) # 개발 시에만 reload
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False) # 개발 시에만 reload
