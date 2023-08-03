54252678ef4a8961400905cded6a62db5a1ac9c7
jjaegii <hn06038@gmail.com>
2023-08-03 16:59:07 +0900

    [🔥 DEL] generate 라우터 삭제

--- BE/main.py
+++ BE/main.py
@@ -4,7 +4,6 @@ from starlette.middleware.cors import CORSMiddleware
 
 from app.user import user_router
 from app.email import email_router
-from app.generate import generate_router
 
 app = FastAPI()
 
@@ -22,7 +21,6 @@ app.add_middleware(
 
 app.include_router(user_router, prefix='/user')
 app.include_router(email_router, prefix='/email')
-app.include_router(generate_router, prefix='/generate')
 
 @app.get("/")
 async def index():
